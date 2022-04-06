2ed79aa1e8007120342fd3227250ee62d500613c
wholemilk <wholemilk25@gmail.com>
2022-04-07 03:15:05 +0900

    kakomon1-brushup

--- kakomon1.py
+++ kakomon1.py
@@ -1,23 +1,15 @@
-# input
-from multiprocessing.connection import answer_challenge
+def solve(): # 解決処理
+  N=int(input()) # F,Xの入力回数
+  FX={}
+  for i in range(N):
+    f, x = input().split()
+    FX.setdefault(f,[]).append(int(x)) # keyが同じなら追加で値が格納される
+  sorted_ans=sorted(FX.items()) # keyをアルファベット順に並べる
+  return sorted_ans
 
-
-N=int(input())
-FX={}
-for i in range(N):
-  f, x = input().split()
-  FX.setdefault(f,[]).append(int(x))
-
-ans={}
-for key in FX.keys():
-  X=key
-  L=min(FX[key])
-  H=max(FX[key])
-  M=sum(FX[key])//len(FX[key])
-  ans[X]=[L,H,M]
-
-sorted_ans=sorted(ans.items())
-
-# output
-print(sorted_ans)
-
+T=int(input()) # caseの数
+for X in range(T):
+  ans=solve()
+  print("Case #{}".format(X+1)) # format関数
+  for i in ans:
+    print (i[0]," ",min(i[1])," ",max(i[1])," ",(sum(i[1])//len(i[1])))
---
+++ memo.md
@@ -0,0 +1,9 @@
+### format
+
+```py
+"Case #{}".format(X+1)
+
+"名前{myname}です。{myold}歳です。".format(myname="Yamada", myold=18)
+```
+
+{}部分に値が入ってくる！
